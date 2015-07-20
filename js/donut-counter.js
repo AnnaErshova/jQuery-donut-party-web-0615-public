8a096137262dc55369c6f43fd8ec981758f19e66
AnnaErshova <anna.ershova@gmail.com>
2015-07-20 17:19:08 -0400

    all done

--- js/donut-counter.js
+++ js/donut-counter.js
@@ -1,5 +1,58 @@
-$(function(){ 
+$(function() { 
   var donutCounter = new DonutCounter();
 });
 
-// your code here
+function DonutCounter() {
+  this.addListenerForPlus();
+  this.addListenerForMinus();
+  this.getStatus();
+};
+
+DonutCounter.prototype.addListenerForPlus = function(){
+  $('.add').click(function(){
+    var currentCount = $(this).parent().find('h3 span').text();
+    var newCount = parseInt(currentCount) + 1;
+    $(this).parent().find('h3 span').text(newCount);
+  });
+};
+
+DonutCounter.prototype.addListenerForMinus = function(){
+  $('.remove').click(function(){
+    var currentCount = $(this).parent().find('h3 span').text();
+    if (currentCount > 0) 
+    {
+      var newCount = parseInt(currentCount) - 1;
+      $(this).parent().find('h3 span').text(newCount);
+    };
+  });
+};
+
+DonutCounter.prototype.getStatus = function(){
+
+  $('#check').click(function(){
+
+    var guestCount = $('.guest-count').text(),
+        donutCount = $('.donut-count').text();
+
+    if (guestCount > donutCount) {
+      $('#status').text('Guests are sad :(')
+    }
+    else if (guestCount == 0)
+    {
+      $('#status').text('Invite someone you jerk!')
+    }
+    else if (guestCount <= donutCount) 
+    {
+      if (donutCount / guestCount >= 4) 
+      {
+        $('#status').text('ARE YOU TRYING TO KILL US ALL!?')
+      }
+      else 
+      {
+        $('#status').text('Guests are having a blast!')
+      }
+    }
+  })
+};
+
+
